3411ed61d69bc115a2ff95c2e6dae4bdfc000413
SaurabhK05 <saurabh.k150600@gmail.com>
2023-06-21 01:53:06 +0530

    Styled second div in landing page

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -19,9 +19,39 @@ const LandingPage = () => {
         <div className="landing-page-div2">
           <div className="landing-page-left-div">
             <p className="landing-page-heading">Looking For a Car ?</p>
-            <div className="dream-car-search"></div>
+            <div className="dream-car-search">
+              <p className="search-your-car">Search you dream car !</p>
+              <div className="search-car-drop-down">
+                <select name="pets" id="pet-select">
+                  <option value="">Select Budget</option>
+                  <option value="dog">Dog</option>
+                </select>
+                <select name="pets" id="pet-select">
+                  <option value="">Select Brand</option>
+                  <option value="dog">Dog</option>
+                </select>
+                <select name="pets" id="pet-select">
+                  <option value="">Select Vehicle</option>
+                  <option value="dog">Dog</option>
+                </select>
+              </div>
+              <button className="search-car-btn">Search</button>
+            </div>
           </div>
-          <div className="landing-page-right-div"></div>
+          <div className="landing-page-right-div">
+            <img
+              src="/Assets/looking-for-car-img.png"
+              alt="Looking for Car Image"
+            />
+          </div>
+        </div>
+        <div className="landing-page-div3">
+          <p className="easy-steps">3 Easy Steps !</p>
+          <div className="three-easy-step">
+            <div className="variety-of-options"></div>
+            <div className="choose-the-one"></div>
+            <div className="easy-pay"></div>
+          </div>
         </div>
       </div>
     </>
