43757de331b7465ed3065b4edab6773b382659cc
Pranav Srisrimal <pranavsrisrimal@gmail.com>
2023-06-22 22:34:47 +0530

    refactor : folder and file name changes

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const home = () => {
+  return (
+    <>
+      <div className="home">
+        <h1>Home Page</h1>
+      </div>
+    </>
+  );
+};
+
+export default home;
