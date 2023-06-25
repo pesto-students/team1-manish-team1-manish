6beb36a1f5d218f10d4c62a9b919d130826fecb5
Hritik Singh <hritiks127@gmail.com>
2023-06-25 20:28:30 +0530

    Image, overview and brief section added

--- src/Pages/CarDetails/CarDetails.jsx
+++ src/Pages/CarDetails/CarDetails.jsx
@@ -12,10 +12,28 @@ const CarDetails = ({ carId }) => {
   const [carData, setCarData] = useState(null);
   const [isBookMarked, setIsBookMarked] = useState(false);
   const [isImageFading, setIsImageFading] = useState([0, true]);
+  let timeOutId = null;
   const bookmarkToggle = () => {
     setIsBookMarked((value) => !value);
   };
+  const manualNextImage = () => {
+    clearTimeout(timeOutId);
+    if (isImageFading[0] >= carData.Images.length - 1) {
+      setIsImageFading([0, false]);
+    } else {
+      setIsImageFading((value) => [value[0] + 1, false]);
+    }
+  };
+  const manualPrevImage = () => {
+    clearTimeout(timeOutId);
+    if (isImageFading[0] <= 0) {
+      setIsImageFading([carData.Images.length - 1, false]);
+    } else {
+      setIsImageFading((value) => [value[0] - 1, false]);
+    }
+  };
   useEffect(() => {
+    clearTimeout(timeOutId);
     if (!carData) {
       //Backend API call for fetching carDetails
       console.log(carId);
@@ -23,8 +41,8 @@ const CarDetails = ({ carId }) => {
         setCarData(CarDetailsData[carId]);
       }, 2000);
     } else {
-      setTimeout(() => {
-        if (isImageFading[0] == carData.Images.length - 2) {
+      timeOutId = setTimeout(() => {
+        if (isImageFading[0] >= carData.Images.length - 1) {
           setIsImageFading([0, !isImageFading[1]]);
         } else {
           setIsImageFading((value) => [value[0] + 1, !value[1]]);
@@ -84,6 +102,12 @@ const CarDetails = ({ carId }) => {
                 </ThemeProvider>
               </div>
               <div className="car-photos">
+                <div className="next-image" onClick={manualNextImage}>
+                  <Icon icon="ic:round-greater-than" />
+                </div>
+                <div className="prev-image" onClick={manualPrevImage}>
+                  <Icon icon="ic:round-less-than" />
+                </div>
                 <img
                   className={isImageFading[1] ? "easeload" : ""}
                   src={carData.Images[isImageFading[0]]}
@@ -92,9 +116,76 @@ const CarDetails = ({ carId }) => {
             </>
           )}
         </div>
-        <div className="car-overview"></div>
-        <div className="car-features"></div>
-        <div className="car-specifications"></div>
+        {!carData ? (
+          <>
+            <div className="shimmer-body">
+              <h3 className="div-header">Car Overview</h3>
+              <div className="shimmering-effect"></div>
+              <div className="shimmering-effect"></div>
+              <div className="shimmering-effect"></div>
+              <div className="shimmering-effect"></div>
+              <div className="shimmering-effect"></div>
+            </div>
+            <div className="shimmer-body">
+              <h3 className="div-header">Features</h3>
+              <div className="shimmering-effect"></div>
+              <div className="shimmering-effect"></div>
+              <div className="shimmering-effect"></div>
+            </div>
+            <div className="shimmer-body">
+              <h3 className="div-header">Specifications</h3>
+              <div className="shimmering-effect"></div>
+              <div className="shimmering-effect"></div>
+              <div className="shimmering-effect"></div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="info-body">
+              <h3 className="div-header">Car Overview</h3>
+              <div className="info-content">
+                <div className="overview-content">
+                  <div className="content-key">Registration Year</div>
+                  <div className="content-key">Fuel Type</div>
+                  <div className="content-key">Kms Driven</div>
+                  <div className="content-key">Ownership</div>
+                  <div className="content-key">Transmission</div>
+                </div>
+                <div className="overview-content">
+                  <div className="content-value">
+                    {carData.RegistrationYear}
+                  </div>
+                  <div className="content-value">{carData.FualType}</div>
+                  <div className="content-value">{carData.KmDriven}</div>
+                  <div className="content-value">{carData.Ownership}</div>
+                  <div className="content-value">{carData.Transmission}</div>
+                </div>
+                <div className="overview-content">
+                  <div className="content-key">Insaurance</div>
+                  <div className="content-key">Variant</div>
+                  <div className="content-key">RTO</div>
+                  <div className="content-key">Engine</div>
+                  <div className="content-key">Year of Manufacture</div>
+                </div>
+                <div className="overview-content">
+                  <div className="content-value">{carData.Insaurance}</div>
+                  <div className="content-value">{carData.Variant}</div>
+                  <div className="content-value">GJ14</div>
+                  <div className="content-value">{carData.Engine}</div>
+                  <div className="content-value">{carData.Year}</div>
+                </div>
+              </div>
+            </div>
+            <div className="info-body">
+              <h3 className="div-header">Features</h3>
+              <div className="info-content"></div>
+            </div>
+            <div className="info-body">
+              <h3 className="div-header">Specifications</h3>
+              <div className="info-content"></div>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
