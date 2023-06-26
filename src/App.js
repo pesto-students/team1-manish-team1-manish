3329cb2eb23b004df2cc0fc5675fab7d38e17dac
Pranav Srisrimal <pranavsrisrimal@gmail.com>
2023-06-26 23:08:51 +0530

    fix : corrected route

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         {/* <Route path="/login" element={<Login />} /> */}
         {/* <Route path="/register" element={<Register />} /> */}
-        <Route path="/forgot-password" element={<ForgotPassword} />
+        <Route path="/forgot-password" element={<ForgotPassword/>} />
       </Routes>
     </div>
   );
