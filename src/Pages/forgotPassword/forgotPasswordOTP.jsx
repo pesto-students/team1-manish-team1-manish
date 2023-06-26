251e1f9f308db8553f5114be3a06b3b3938f72e6
Pranav Srisrimal <pranavsrisrimal@gmail.com>
2023-06-26 22:57:24 +0530

    feat : password all screens

---
+++ src/Pages/forgotPassword/forgotPasswordOTP.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './styles.css';
+
+const forgotPasswordOTP = () => {
+  return (
+    <>
+      <div className="forgot__password">
+        <h1 className="forgot__password__header">Forgot Password?!</h1>
+        <p className="forgot__password__sub-header">
+          Enter the OTP sent on your registered email ID
+        </p>
+        <div className="forgot_password__otp">
+          <input
+            name="box1"
+            className="forgot__password__otp__box"
+            type="text"
+          ></input>
+          <input
+            name="box2"
+            className="forgot__password__otp__box"
+            type="text"
+          ></input>
+          <input
+            name="box3"
+            className="forgot__password__otp__box"
+            type="text"
+          ></input>
+          <input
+            name="box4"
+            className="forgot__password__otp__box"
+            type="text"
+          ></input>
+          <input
+            name="box5"
+            className="forgot__password__otp__box"
+            type="text"
+          ></input>
+          <input
+            name="box6"
+            className="forgot__password__otp__box"
+            type="text"
+          ></input>
+        </div>
+        <button className="forgot__password__button" type="submit">
+          Submit
+        </button>
+        <p className="forgot__password__footer">
+          Didn't receive?
+          <a href="#">Resend OTP</a>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default forgotPasswordOTP;
---
+++ src/Pages/forgotPassword/successPage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./styles.css";
+
+const successPage = () => {
+  return (
+    <>
+      <div className="forgot__password">
+        <div className="forgot_password__close">X</div>
+        <h1 className="forgot__password__header">Password Updated</h1>
+        <img
+          src="./assets/pw_successful.png"
+          alt="Password reset successful"
+          className="forgot__password__image"
+        />
+        <p className="forgot__password__sub-header">
+          Your password has been updated successfully!
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default successPage;
