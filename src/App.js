ae16a50b1c552c38fbeccf011120be000374dd43
Pranav Srisrimal <pranavsrisrimal@gmail.com>
2023-06-26 23:25:58 +0530

    refactor : removed commented code

--- src/App.js
+++ src/App.js
@@ -19,8 +19,6 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<Login />} /> */}
-        {/* <Route path="/register" element={<Register />} /> */}
         <Route path="/forgot-password" element={<ForgotPassword/>} />
       </Routes>
       <Footer />
